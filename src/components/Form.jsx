4a2ddb02015101dcb329ee5dee4e6331a04f6404
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-05-02 19:48:37 -0300

    Requisito 3 feito

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,30 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from '../context/AppProvider';
 
 export default function Form() {
-  const { nameFilter, setNameFilter } = useContext(AppContext);
+  const [columnFilters, setColumnFilters] = useState({
+    column: 'population',
+    parameter: 'maior que',
+    value: 0,
+  });
+
+  const { nameFilter, setNameFilter, filterPlanets } = useContext(AppContext);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setColumnFilters({
+      ...columnFilters,
+      [name]: value,
+    });
+  };
+
+  const handleColumnFilters = (event) => {
+    event.preventDefault();
+    filterPlanets(columnFilters);
+  };
+
   return (
-    <form>
+    <form onSubmit={ handleColumnFilters }>
       <label>
         <input
           type="text"
@@ -14,6 +34,55 @@ export default function Form() {
           onChange={ ({ target }) => setNameFilter(target.value) }
         />
       </label>
+
+      <label htmlFor="column">
+        Coluna:
+        <select
+          name="column"
+          id="column"
+          data-testid="column-filter"
+          value={ columnFilters.column }
+          onChange={ handleChange }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+
+      <label htmlFor="parameter">
+        Operador:
+        <select
+          name="parameter"
+          id="parameter"
+          data-testid="comparison-filter"
+          value={ columnFilters.parameter }
+          onChange={ handleChange }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+
+      <label>
+        <input
+          type="number"
+          data-testid="value-filter"
+          name="value"
+          value={ columnFilters.value }
+          onChange={ handleChange }
+        />
+      </label>
+
+      <button
+        type="submit"
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
     </form>
   );
 }
--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { createContext, useEffect, useMemo, useState } from 'react';
+import { createContext, useEffect, useMemo, useState, useCallback } from 'react';
 
 export const AppContext = createContext();
 
@@ -8,7 +8,7 @@ const URL = 'https://swapi.dev/api/planets';
 export default function AppProvider({ children }) {
   const [apiData, setApiData] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
-  // const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -23,12 +23,26 @@ export default function AppProvider({ children }) {
     fetchApi();
   }, []);
 
+  const filterPlanets = useCallback(({ column, parameter, value }) => {
+    let filtered = [];
+    if (parameter.includes('maior que')) {
+      filtered = apiData.filter((planet) => Number(planet[column]) > Number(value));
+    } else if (parameter.includes('menor que')) {
+      filtered = apiData.filter((planet) => Number(planet[column]) < Number(value));
+    } else if (parameter.includes('igual a')) {
+      filtered = apiData.filter((planet) => Number(planet[column]) === Number(value));
+    }
+    setFilteredPlanets(filtered);
+  }, [apiData]);
+
   const context = useMemo(() => ({
     apiData,
     setApiData,
     nameFilter,
     setNameFilter,
-  }), [apiData, setApiData, nameFilter, setNameFilter]);
+    filterPlanets,
+    filteredPlanets,
+  }), [apiData, setApiData, nameFilter, setNameFilter, filterPlanets, filteredPlanets]);
 
   return (
     <AppContext.Provider value={ context }>
